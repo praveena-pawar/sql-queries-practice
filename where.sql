03e77235796e588fa381d967f34f3f8743cb1874
praveena-pawar <praveena.dev02@gmail.com>
2025-12-02 10:54:11 +0530

    query using where clause

--- where.sql
+++ where.sql
@@ -4,3 +4,8 @@
 -- Display employees from the IT department with salary above 60,000.
 SELECT * FROM employees 
 WHERE department = 'it' and salary > 60000;
+
+
+-- Display employees whose age is between 25 and 30.
+SELECT * FROM employees 
+WHERE age  BETWEEN 25 and 30;	
